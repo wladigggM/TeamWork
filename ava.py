4b0d3e2de3ae405fa8adbc569e03206ddd9cbc3e
New85 <102480221+New85@users.noreply.github.com>
2023-10-14 10:45:33 +0300

    Update ava.py

--- ava.py
+++ ava.py
@@ -1,8 +1,7 @@
-import os
+
 
 ava = input("Введите ссылку на ваш аватар ")
-print(os.getcwd())
-with open(f"{ava}", "rb") as f:
+
+with open(ava, "rb") as f:
     bin_file = f.read()
 print(bin_file)
-#print(os.getcwd())
